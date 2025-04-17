ff11ccad5c4006c1aa3245157cc04add6d2bb8c5
diatsilva007 <diatsilva007@gmail.com>
2025-04-17 15:05:23 -0300

    Adicionar funcionalidade de busca de usuários e página 'quem seguir'

--- App/Controllers/AppController.php
+++ App/Controllers/AppController.php
@@ -51,6 +51,27 @@ class AppController extends Action {
 		}	
 
 	}
+
+	public function quemSeguir() {
+
+		$this->validaAutenticacao();
+
+		$pesquisarPor = isset($_GET['pesquisarPor']) ? $_GET['pesquisarPor'] : '';
+		
+		$usuarios = array();
+
+		if($pesquisarPor != '') {
+			
+			$usuario = Container::getModel('Usuario');
+			$usuario->__set('nome', $pesquisarPor);
+			$usuarios = $usuario->getAll();
+
+		}
+
+		$this->view->usuarios = $usuarios;
+
+		$this->render('quemSeguir');
+	}	
 }
 
 ?>
